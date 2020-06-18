1cf53591ee38939f7ee82cebee62009bae01aa81
liyuchen <liyuchen@changba.com>
2020-06-18 14:26:07 +0800

    for test

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import gevent.monkey
 os.environ['TZ'] = 'Asia/Shanghai'
 gevent.monkey.patch_all()
 
+'''
 def run(stock, notifyUrl):
     watcher = Watcher(stock['code'], buyPriceList=stock['buyPriceList'], salePriceList=stock['salePriceList'], notifyUrl=notifyUrl)
     watcher.start()
@@ -26,3 +27,7 @@ if __name__ == '__main__':
         t = gevent.spawn(run, stock, conf.reload().data['notifyUrl'])
         taskList.append(t)
     gevent.joinall(taskList)
+'''
+
+watcher = Watcher('sh600398', buyPriceList=[5.98], salePriceList=[6.20], notifyUrl='https://oapi.dingtalk.com/robot/send?access_token=d29bc5f270e97303817f9a0e3df8ccfa3e31efc04b04c8c2854765affb5b3fd9')
+watcher.start()
