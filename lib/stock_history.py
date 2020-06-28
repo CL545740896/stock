5f40f6ad6aea5173380dc67df01dfa9cd277284d
liyuchen <liyuchen@changba.com>
2020-06-28 13:52:13 +0800

    modify

---
+++ lib/stock_history.py
@@ -0,0 +1,79 @@
+#coding=utf-8
+
+'''
+获取股票的历史数据
+'''
+
+from lib.point import Point
+import requests
+import demjson
+
+class StockHistory:
+
+    def __init__(self, code, startDate, endDate):
+        self.code = code
+        self.startDate = startDate
+        self.endDate = endDate
+        self.baseurl = 'https://q.stock.sohu.com'
+        self.timeout = 30
+
+    def transCode(self, code):
+        code = str(code).lower().replace('sz', '').replace('sh', '')
+        code = 'cn_' + code
+        return code
+
+    def makeUrl(self):
+        url =  self.baseurl + '/hisHq?code=%s&start=%s&end=%s&stat=1&order=A&period=d&callback=historySearchHandler&rt=json' % (
+            self.transCode(self.code), self.startDate, self.endDate
+        )
+        return url
+    
+    def fetchRaw(self):
+        '''
+        成功返回响应内容和None, 否则返回响应内容和错误信息
+        '''
+        url = self.makeUrl()
+        resp = ''
+        err = None
+        try:
+            r = requests.get(url, timeout = self.timeout, verify = False)
+            code, resp = r.status_code, r.text
+            if code == 200:
+                return resp, None
+            else:
+                return resp, "http code:%s" % code
+        except Exception as ex:
+            return resp, str(ex)
+
+    def getPointList(self):
+        '''
+        成功返回list和None, 否则返回list和错误信息
+        '''
+        pointList = []
+        err = None
+        raw, err = self.fetchRaw()
+        raw = raw.strip()
+        if err != None:
+            return pointList, err
+        try:
+            data = demjson.decode(raw)
+            data = data[0]['hq']
+            for dayData in data:
+                p = Point()
+                # 0    1    2    3    4    5    6    7       8    9
+                #日期，开盘，收盘，涨跌，涨幅，最低，最高，成交量，成交额，换手
+                p.time = dayData[0]
+                p.dayBegin = dayData[1]
+                p.dayEnd = dayData[2]
+                p.dayMin = dayData[5]
+                p.dayMax = dayData[6]
+                pointList.append(p)
+        except Exception as ex:
+            err = str(ex)
+        return pointList, err
+
+sh = StockHistory('sz159919', 20200601, 20200621)
+pointList, err = sh.getPointList()
+if err != None: import sys;print(err);sys.exit(0)
+for p in pointList:
+    print(p.time, p.dayEnd)
--- main.py
+++ main.py
@@ -12,22 +12,34 @@ import time
 import os
 from agileutil.queue import UniMemQueue
 from agileutil.log import Log
+from lib.stock_history import StockHistory
+import agileutil.wrap as awrap
 os.environ['TZ'] = 'Asia/Shanghai'
 commonLogger = Log('./common.log')
 
-
+@awrap.safe
 def run(stock, notifyUrl):
     watcher = Watcher(stock['code'], buyPriceList=stock['buyPriceList'], salePriceList=stock['salePriceList'], notifyUrl=notifyUrl)
     watcher.setCommonLogger(commonLogger)
     watcher.start()
 
+@awrap.safe
 def push_msg():
     while 1:
-        time.sleep(1)
+        time.sleep(5)
         msg = UniMemQueue.getInstance().pop()
         if msg == None:
             continue
         lib.notify.safeSendDDMsg(conf.reload().data['notifyUrl'], msg)
+
+@awrap.safe
+def gen_report():
+    while 1:
+        for stock in conf.reload().data['stockList']:
+            sh = StockHistory(stock['code'], '20200601', '20200623')
+            pointList = sh.getPointList()
+            print(pointList)
+            time.sleep(1)
 
 if __name__ == '__main__':
     conf = Config("./config.json")
@@ -42,5 +54,9 @@ if __name__ == '__main__':
     #推送买入、卖出消息
     t = gevent.spawn(push_msg)
     taskList.append(t)
+
+    #生成最近15个交易日的这线图
+    #t = gevent.spawn(gen_report)
+    #taskList.append(t)
 
     gevent.joinall(taskList)
