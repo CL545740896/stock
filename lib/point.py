5a7bbe4f061e0f7aa8f19d194821ab4e5a822e16
liyuchen <liyuchen@changba.com>
2020-08-04 19:44:45 +0800

    移除调试代码

--- lib/point.py
+++ lib/point.py
@@ -58,7 +58,7 @@ class Point:
         '''
         判断当前是否为A股交易时间
         '''
-        return True
+        #return True
         curStamp = time.time()
         t = time.localtime(curStamp)
         if t.tm_wday not in [0, 1, 2, 3, 4]: return False
